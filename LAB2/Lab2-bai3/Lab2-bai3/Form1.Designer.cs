4d50c5c7aaff6c940692040a51780bbe9d3f376e
Longdang289 <133092650+Longdang289@users.noreply.github.com>
2024-10-09 15:44:13 +0700

    lab2 bài 3

---
+++ LAB2/Lab2-bai3/Lab2-bai3/Form1.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace Lab2_bai3
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            calculate = new Button();
+            input = new RichTextBox();
+            openFile = new Button();
+            output = new RichTextBox();
+            inputPath = new TextBox();
+            outputPath = new TextBox();
+            SuspendLayout();
+            // 
+            // calculate
+            // 
+            calculate.Location = new Point(353, 246);
+            calculate.Name = "calculate";
+            calculate.Size = new Size(94, 29);
+            calculate.TabIndex = 0;
+            calculate.Text = "Enter";
+            calculate.UseVisualStyleBackColor = true;
+            calculate.Click += calculate_Click;
+            // 
+            // input
+            // 
+            input.Location = new Point(12, 39);
+            input.Name = "input";
+            input.Size = new Size(335, 399);
+            input.TabIndex = 1;
+            input.Text = "";
+            // 
+            // openFile
+            // 
+            openFile.Location = new Point(353, 142);
+            openFile.Name = "openFile";
+            openFile.Size = new Size(94, 29);
+            openFile.TabIndex = 3;
+            openFile.Text = "Open File";
+            openFile.UseVisualStyleBackColor = true;
+            // 
+            // output
+            // 
+            output.Location = new Point(453, 39);
+            output.Name = "output";
+            output.Size = new Size(335, 399);
+            output.TabIndex = 4;
+            output.Text = "";
+            // 
+            // inputPath
+            // 
+            inputPath.Location = new Point(12, 6);
+            inputPath.Name = "inputPath";
+            inputPath.Size = new Size(335, 27);
+            inputPath.TabIndex = 5;
+            // 
+            // outputPath
+            // 
+            outputPath.Location = new Point(453, 6);
+            outputPath.Name = "outputPath";
+            outputPath.Size = new Size(335, 27);
+            outputPath.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(outputPath);
+            Controls.Add(inputPath);
+            Controls.Add(output);
+            Controls.Add(openFile);
+            Controls.Add(input);
+            Controls.Add(calculate);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button calculate;
+        private RichTextBox input;
+        private Button openFile;
+        private RichTextBox output;
+        private TextBox inputPath;
+        private TextBox outputPath;
+    }
+}
---
+++ LAB2/Lab2-bai3/Lab2-bai3/Form1.cs
@@ -0,0 +1,159 @@
+﻿﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Lab2_bai3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            openFile.Click += openFile_Click;
+        }
+
+        OpenFileDialog opd;
+        private void openFile_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                opd = new OpenFileDialog();
+                opd.Filter = "Text Files |*.txt";
+                opd.ShowDialog();
+                FileStream fs = new FileStream(opd.FileName, FileMode.OpenOrCreate);
+                StreamReader sr = new StreamReader(fs, Encoding.UTF8);
+                input.Text = sr.ReadToEnd();
+                sr.BaseStream.Position = 0;
+                inputPath.Text = fs.Name;
+                calculate.Enabled = true;
+                fs.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+        }
+        private void calculate_Click(object sender, EventArgs e)
+        {
+            File.WriteAllText("..\\output3.txt", "");
+            FileStream fsw = new FileStream("..\\output3.txt", FileMode.OpenOrCreate);
+            StreamWriter sw = new StreamWriter(fsw, Encoding.UTF8);
+
+            FileStream fsr = new FileStream(opd.FileName, FileMode.OpenOrCreate);
+            StreamReader sr = new StreamReader(fsr, Encoding.UTF8);
+
+            string[] res = Calc(sr);
+            foreach (var item in res)
+            {
+                sw.WriteLine(item);
+            }
+            sw.Close();
+            output.Text = File.ReadAllText(fsw.Name);
+            outputPath.Text = fsw.Name;
+            fsw.Close();
+            fsr.Close();
+        }
+        private string[] Calc(StreamReader sr)
+        {
+            try
+            {
+                var content = sr.ReadToEnd();
+
+                // Thay thế tất cả các dấu trừ dài (–) bằng dấu trừ ngắn (-)
+                content = content.Replace("–", "-");
+
+                var Lines = content.Split(new string[] { "\r\n" }, StringSplitOptions.None);
+                for (int i = 0; i < Lines.Length; i++)
+                {
+                    if (Lines[i].Contains('+') || Lines[i].Contains('-'))
+                    {
+                        string temp = Lines[i];
+                        var calc = Lines[i].Split(new char[] { '-', '+' }); ;
+                        float res = 0;
+                        for (int j = 0; j < calc.Length; j++)
+                        {
+                            if (Lines[i].IndexOf('-') != -1 && Lines[i].IndexOf('-') - Lines[i].IndexOf(calc[j]) == -1)
+                            {
+                                int indexOfSo = Lines[i].IndexOf(calc[j]);
+                                int indexOfDau = Lines[i].IndexOf('-');
+
+                                if (indexOfSo != -1)
+                                {
+                                    int lengthofSo = calc[j].Length;
+                                    Lines[i] = Lines[i].Remove(indexOfSo, lengthofSo);
+                                }
+
+                                if (indexOfDau != -1 && indexOfDau - (indexOfSo) == -1)
+                                {
+                                    indexOfSo = Lines[i].IndexOf(calc[j]);
+                                    Lines[i] = Lines[i].Remove(indexOfDau, 1);
+                                }
+
+                                res -= float.Parse(calc[j]);
+                            }
+                            else
+                            {
+                                int indexOfSo = Lines[i].IndexOf(calc[j]);
+                                int indexOfDau = Lines[i].IndexOf('+');
+
+                                if (indexOfSo != -1)
+                                {
+                                    int lengthofSo = calc[j].Length;
+                                    Lines[i] = Lines[i].Remove(indexOfSo, lengthofSo);
+                                }
+
+                                if (indexOfDau != -1 && indexOfDau - (indexOfSo) == -1)
+                                {
+                                    indexOfSo = Lines[i].IndexOf(calc[j]);
+                                    Lines[i] = Lines[i].Remove(indexOfDau, 1);
+                                }
+
+                                res += float.Parse(calc[j]);
+                            }
+                        }
+                        Lines[i] = temp + " = " + res.ToString();
+                    }
+                    else if (Lines[i].Contains('*'))
+                    {
+                        var calc = Lines[i].Split('*');
+                        float res = 1;
+                        for (int j = 0; j < calc.Length; j++)
+                        {
+                            res *= float.Parse(calc[j]);
+                        }
+                        Lines[i] = Lines[i] + " = " + res.ToString();
+                    }
+                    else if (Lines[i].Contains('/'))
+                    {
+                        var calc = Lines[i].Split('/');
+                        float res = 1;
+                        for (int j = 0; j < calc.Length; j++)
+                        {
+                            res /= float.Parse(calc[j]);
+                        }
+                        Lines[i] = Lines[i] + " = " + res.ToString();
+                    }
+                    else
+                    {
+                        MessageBox.Show("Wrong Input!");
+                    }
+                }
+                return Lines;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                return new string[] { "Nothing" };
+            }
+        }
+
+
+        private void Bai3_Load(object sender, EventArgs e)
+        {
+            calculate.Enabled = false;
+        }
+    }
+}
